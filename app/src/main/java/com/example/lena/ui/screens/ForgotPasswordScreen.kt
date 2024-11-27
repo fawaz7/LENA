2c3a68b809dd9c82d9dfb48dfbaee1a2a9101a69
Fawaz <ghazawe1@gmail.com>
2024-11-28 01:07:48 +0300

    # LENA: Linguistic Engine Natural Assistant
    27/11/2024 @ 05:14PM
    
    * Forgot Password Functionality working

--- app/src/main/java/com/example/lena/ui/screens/ForgotPasswordScreen.kt
+++ app/src/main/java/com/example/lena/ui/screens/ForgotPasswordScreen.kt
@@ -7,8 +7,10 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
@@ -38,9 +40,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -87,9 +86,7 @@ fun ForgotPasswordScreen(
         label = "TextSize Animation")
     val subTextSize by animateFloatAsState(targetValue = if (isImeVisible) 20f else 16f, animationSpec = tween(durationMillis = 300),
         label = "subTextSize Animation")
-    val spacerHeight by animateDpAsState(targetValue = if (isImeVisible) 0.dp else 40.dp, animationSpec = tween(durationMillis = 300),
-        label = "SpacerHeight Animation")
-    val firstSpacerHeight by animateDpAsState(targetValue = if (isImeVisible) 20.dp else 64.dp, animationSpec = tween(durationMillis = 300),
+    val firstSpacerHeight by animateDpAsState(targetValue = if (isImeVisible) 8.dp else 24.dp, animationSpec = tween(durationMillis = 300),
         label = "firstSpacerHeight Animation")
 
     Scaffold(
@@ -118,16 +115,15 @@ fun ForgotPasswordScreen(
 
         Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .padding(innerPadding)
                 .pointerInput(Unit) { detectTapGestures(onTap = { focusManager.clearFocus() }) },
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ){
-            Spacer(modifier = modifier.height(firstSpacerHeight).fillMaxWidth())
 
             Crossfade(targetState = isImeVisible, label = "Welcome Screen Label Animation") { isKeyboardVisible ->
-                Column(modifier.align(Alignment.CenterHorizontally).padding(32.dp)) {
-                    Spacer(modifier.height(spacerHeight))
+                Column(modifier.align(Alignment.CenterHorizontally).padding(horizontal = 32.dp)) {
                     Text(
                         text = "Forgot Password?",
                         fontWeight = if (!isKeyboardVisible) FontWeight.Bold else FontWeight.Light,
@@ -147,7 +143,7 @@ fun ForgotPasswordScreen(
             Spacer(modifier = modifier.height(firstSpacerHeight).fillMaxWidth())
             Column(
                 modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp).imePadding(),
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 OutlinedTextField(
                     value = uiState.forgotPasswordEmail,
@@ -190,7 +186,6 @@ fun ForgotPasswordScreen(
                     )
                 )
                 //=======================================================================================---> End of Text Field
-                Spacer(modifier = Modifier.height(16.dp))
                 Button(
                     onClick = {
                         viewModel.onForgotPasswordEmailFocusChanged(false)
@@ -210,7 +205,7 @@ fun ForgotPasswordScreen(
                         disabledContainerColor = if (isSystemInDarkTheme()) Gray900 else MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
                         disabledContentColor = if (isSystemInDarkTheme()) Gray800 else MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f),
                     ),
-                    modifier = modifier.padding(8.dp).width(124.dp),
+                    modifier = modifier.width(124.dp),
                     shape = RoundedCornerShape(16.dp)
                 ) {
                     if (authState.value == AuthState.Loading) {
